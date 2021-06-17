09c36fbf64aebae0daa9e0225544f84be3611095
Kalande Mwamba <kalandekupe@outlook.com>
2021-06-17 22:23:07 +0200

    Music Added

---
+++ JS/images.js
@@ -0,0 +1,169 @@
+let bg = new Image()
+bg.src = './Images/background2.png'
+
+let fg = new Image()
+fg.src = './Images/foreground.png'
+
+let platform = new Image()
+platform.src = './Images/platform.png'
+
+let paper = new Image()
+paper.src = './Images/collectable.png'
+
+let startScr = new Image()
+startScr.src = './Images/start.png'
+
+let endScr = new Image()
+endScr.src = './Images/Game over.png'
+
+let introScr = new Image()
+introScr.src = './Images/Intro.png'
+
+//Idle Right
+let charIdle1 = new Image()
+charIdle1.src = './Images/Sprites/Idle/Right/idle1.png'
+
+let charIdle2 = new Image()
+charIdle2.src = './Images/Sprites/Idle/Right/idle2.png'
+
+let charIdle3 = new Image()
+charIdle3.src = './Images/Sprites/Idle/Right/idle3.png'
+
+let charIdle4 = new Image()
+charIdle4.src = './Images/Sprites/Idle/Right/idle4.png'
+
+let charIdleR = [charIdle1, charIdle2, charIdle3, charIdle4]
+
+//Idle Left 
+let charIdleL1 = new Image()
+charIdleL1.src = 'Images/Sprites/Idle/Left/idle1 left.png'
+
+let charIdleL2 = new Image()
+charIdleL2.src = 'Images/Sprites/Idle/Left/idle2 left.png'
+
+let charIdleL3 = new Image()
+charIdleL3.src = 'Images/Sprites/Idle/Left/idle3 left.png'
+
+let charIdleL4 = new Image()
+charIdleL4.src = 'Images/Sprites/Idle/Left/idle4 left.png'
+
+let charIdleL = [charIdleL1, charIdleL2, charIdleL3, charIdleL4]
+
+//Running Right
+let charR1 = new Image()
+charR1.src = 'Images/Sprites/Running/Right/running1.png'
+
+let charR2 = new Image()
+charR2.src = 'Images/Sprites/Running/Right/running2.png'
+
+let charR3 = new Image()
+charR3.src = 'Images/Sprites/Running/Right/running3.png'
+
+let charR4 = new Image()
+charR4.src = 'Images/Sprites/Running/Right/running4.png'
+
+let charRight = [charR1, charR2, charR3, charR4]
+
+//Running Left
+let charL1 = new Image()
+charL1.src = 'Images/Sprites/Running/Left/running1 left.png'
+
+let charL2 = new Image()
+charL2.src = 'Images/Sprites/Running/Left/running2 left.png'
+
+let charL3 = new Image()
+charL3.src = 'Images/Sprites/Running/Left/running3 left.png'
+
+let charL4 = new Image()
+charL4.src = 'Images/Sprites/Running/Left/running4 left.png'
+
+let charLeft = [charL1, charL2, charL3, charL4]
+
+//Jump right
+let charJumpR1 = new Image()
+charJumpR1.src = 'Images/Sprites/Jumping/Right/jumping2.png'
+
+let charJumpR2 = new Image()
+charJumpR2.src = 'Images/Sprites/Jumping/Right/jumping3.png'
+
+let charJumpR3 = new Image()
+charJumpR3.src = 'Images/Sprites/Jumping/Right/jumping4.png'
+
+let charJumpR = [charJumpR1, charJumpR2, charJumpR3]
+
+//Jump left
+let charJumpL1 = new Image()
+charJumpL1.src = 'Images/Sprites/Jumping/Left/jumping2 left.png'
+
+let charJumpL2 = new Image()
+charJumpL2.src = 'Images/Sprites/Jumping/Left/jumping3 left.png'
+
+let charJumpL3 = new Image()
+charJumpL3.src = 'Images/Sprites/Jumping/Left/jumping4 left.png'
+
+let charJumpL = [charJumpL1, charJumpL2, charJumpL3]
+
+//cars
+
+let carL1 = new Image()
+carL1.src = 'Images/cars/Car L1.png'
+
+let carL2 = new Image()
+carL2.src = 'Images/cars/Car L2.png'
+
+let carL3 = new Image()
+carL3.src = 'Images/cars/Car L3.png'
+
+let carR1 = new Image()
+carR1.src = 'Images/cars/Car R1.png'
+
+let carR2 = new Image()
+carR2.src = 'Images/cars/Car R2.png'
+
+let carR3 = new Image()
+carR3.src = 'Images/cars/Car R3.png'
+
+let cars = [carR1,carR2,carR3,carL1,carL2,carL3]
+
+//boss sprites
+let b1 = new Image()
+b1.src = 'Images/Sprites/boss/boss 1.png'
+
+let b2 = new Image()
+b2.src = 'Images/Sprites/boss/boss 2.png'
+
+let b3 = new Image()
+b3.src = 'Images/Sprites/boss/boss 3.png'
+
+let b4 = new Image()
+b4.src = 'Images/Sprites/boss/boss 4.png'
+
+let b5 = new Image()
+b5.src = 'Images/Sprites/boss/boss 5.png'
+
+let b6 = new Image()
+b6.src = 'Images/Sprites/boss/boss 6.png'
+
+let b7 = new Image()
+b7.src = 'Images/Sprites/boss/boss 7.png'
+
+let b8 = new Image()
+b8.src = 'Images/Sprites/boss/boss 8.png'
+
+let b9 = new Image()
+b9.src = 'Images/Sprites/boss/boss 9.png'
+
+let boss = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b1,b2,b3,b4]
+
+//dizzy gameover screen sprite
+
+let dizzy1 = new Image()
+dizzy1.src = 'Images/Sprites/Dizzy/dizzy1.png'
+
+let dizzy2 = new Image()
+dizzy2.src = 'Images/Sprites/Dizzy/dizzy2.png'
+
+let dizzy3 = new Image()
+dizzy3.src = 'Images/Sprites/Dizzy/dizzy3.png'
+
+let dizzy = [dizzy1,dizzy2,dizzy3]
